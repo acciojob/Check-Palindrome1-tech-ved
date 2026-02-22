3c05c53f28f7cd11b4b4a2f27efd373aef803621
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-02-22 23:44:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function isPalindrome(s) {
-  // normalize: remove spaces + lowercase
-  let clean = s.toLowerCase().replace(/\s+/g, "");
-  
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+function isPalindrome(str) {
+  let clean = str.toLowerCase().replace(/[^a-z0-9]/g, "");
   let rev = clean.split("").reverse().join("");
   return clean === rev;
 }
+
+app.post("/palindromechecker", (req, res) => {
+  const { str } = req.body;
+
+  const result = isPalindrome(str);
+  res.status(200).json({ message: result });
+});
+
+module.exports = app;
