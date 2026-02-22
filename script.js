307c4fe184b276ae95ce2c87da258c8b157bb064
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-02-22 23:41:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// complete the given function
-
-function palindrome(str){
-
+function isPalindrome(s) {
+  // normalize: remove spaces + lowercase
+  let clean = s.toLowerCase().replace(/\s+/g, "");
+  
+  let rev = clean.split("").reverse().join("");
+  return clean === rev;
 }
-module.exports = palindrome
